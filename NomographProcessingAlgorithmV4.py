94dc41ed3caf150fda733fc16ecf8b065989ae2a
Evelyn Uuemaa <evelyn.uuemaa@ut.ee>
2025-12-11 15:57:17 +1300

    update figure

--- NomographProcessingAlgorithmV4.py
+++ NomographProcessingAlgorithmV4.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+# Nomograph implementation in Python as QGIS Processing algorithm plugin
 """
 ***************************************************************************
 *                                                                         *
